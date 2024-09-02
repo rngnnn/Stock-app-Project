53216384aeef82a63ed9a88d6d68e46580fbeb63
rngnnn <rngnnnclk@gmail.com>
2024-09-02 12:34:35 +0200

    register page with formik docs

--- inclass-formik-yup/src/pages/Register.jsx
+++ inclass-formik-yup/src/pages/Register.jsx
@@ -4,12 +4,29 @@ import Avatar from "@mui/material/Avatar";
 import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/regi.avif";
 import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
 import { Box } from "@mui/material";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
+import { Formik,Form } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  username:Yup.string().required("Bu alan zorunludur!").min(3,'Username en az 3 karakter olmalıdır!'),
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+});
 
 const Register = () => {
+
   return (
     <Container maxWidth="lg">
       <Grid
@@ -43,6 +60,46 @@ const Register = () => {
           >
             Register
           </Typography>
+
+          <Formik
+            initialValues={{
+              "username": "",
+              "password": "",
+              "email": "",
+              "firstName": "",
+              "lastName": ""
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={values => {
+              // same shape as initial values
+              console.log(values);
+            }}
+          >
+            {
+              ({
+                values,//* state
+                errors,//* errorState i
+                touched, //* focuslanıp focuslanmadığını yakalayan
+                handleChange,//* change ecventında çalışacak olan fonksyon
+                handleBlur,//* blur eventı yani kullanıcının inputdan ayrıldığında çalışacak olan
+                handleSubmit,//* form submit olduğunda
+                isSubmitting,//* başlagnıç false, submite tıklanıldığında true
+                /* and other goodies */
+              }) => (
+                <Form>
+                  <TextField 
+                    name="username"
+                    label="Username"
+                    value={values.username}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.username && errors.username}
+                    helperText={touched.username && errors.username}
+                  />
+                </Form>
+              )
+            }
+          </Formik>
 
           <Box sx={{ textAlign: "center", mt: 2, color:"secondary.main" }}>
             <Link to="/">Already have an account? Sign in</Link>
