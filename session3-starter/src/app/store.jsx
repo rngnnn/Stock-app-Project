80feb7767474459825b6e26f7321a1258ad1bd20
rngnnn <rngnnnclk@gmail.com>
2024-09-06 09:12:11 +0200

    starter

---
+++ session3-starter/src/app/store.jsx
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+// import authMidReducer from "../features/authSliceMiddleware"; //!middleware ile olan kullanım
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+  devTools: process.env.NODE_ENV !== "production",
+});
+export default store;
