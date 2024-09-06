5263941ddb137344c3b832462b8dd4bb47ef1542
rngnnn <rngnnnclk@gmail.com>
2024-09-06 09:25:06 +0200

    persist use

---
+++ inclass-stock-app/src/app/store.jsx
@@ -0,0 +1,27 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+
+//*redux-persist
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+//! reduxtoolkit store
+const store = configureStore({
+  reducer: {
+    // auth: authReducer,
+    auth: persistedReducer,//* kalıcı hale gelmesini istediğimiz reducerı store a verdik.
+  },
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+//! redux-persist
+export let persistor = persistStore(store)
+
+export default store;
---
+++ inclass-stock-app/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div style={{marginTop:"5rem"}}>Home</div>
+  )
+}
+
+export default Home
